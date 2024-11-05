d7296a3cf91723156a532595a0ed557135cdf55b
Johan <johanfendi0112@gmail.com>
2024-11-05 21:11:17 +0100

    Testing of pygame sliders

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ import pygame as pg
 import sys
 
 from algorithms import *
+from maze import *
 
-MAX_WINDOW_WIDTH = 1240
+SIDEBAR_WIDTH = 400
+MAX_WINDOW_WIDTH = 1240 
 MAX_WINDOW_HEIGHT = 630
+MAX_MAZE_GRAPHICS_WIDTH = MAX_WINDOW_WIDTH - SIDEBAR_WIDTH
+
 FPS = 10
 
 
 def get_user_input():
     cell_width = int(input('Enter cell width: '))
     max_maze_height = int(((MAX_WINDOW_HEIGHT / cell_width) - 3)/2)
-    max_maze_width = int(((MAX_WINDOW_WIDTH / cell_width) - 3)/2)
+    max_maze_width = int(((MAX_MAZE_GRAPHICS_WIDTH / cell_width) - 3)/2)
     maze_width = 2*int(input(f'Enter maze_width between 0 and {max_maze_width}: ')) + 3
     maze_height = 2*int(input(f'Enter maze height between 0 and {max_maze_height}: ')) + 3
     max_edge_weight = int(input('Enter max edge weight:'))
     window_height = cell_width * maze_height
-    window_width = cell_width * maze_width
+    window_width = cell_width * maze_width + SIDEBAR_WIDTH
     assert window_width <= MAX_WINDOW_WIDTH, 'WindowWidthError: Window to wide, chose smaller maze width.'
     assert window_height <= MAX_WINDOW_HEIGHT, 'WindowHeightError: Window to tall, chose smaller maze height.'
     assert window_width > 0, 'WindowWidthError: Window to small, chose larger maze width.'
@@ -30,7 +34,7 @@ def update_screen(maze, window):
 
 def check_user_quit(running):
     for event in pg.event.get():
-        if event.type in [pg.QUIT] :
+        if event.type == pg.QUIT :
             running = False
 
     return running
@@ -40,6 +44,7 @@ def main(fps):
     CELL_WIDTH, MAZE_HEIGHT, MAZE_WIDTH, WINDOW_HEIGHT, WINDOW_WIDTH, MAX_EDGE_WEIGHT = get_user_input()
     WINDOW = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     maze = Maze(MAZE_HEIGHT, MAZE_WIDTH, CELL_WIDTH, CELL_WIDTH, MAX_EDGE_WEIGHT, WINDOW, FPS)
+    kruskals(maze, WINDOW, fps)
     running = True
     clock = pg.time.Clock()
     while running:
