560141ec09ba1a8ebeb9f768b6a65f302828ff68
Johan <johanfendi0112@gmail.com>
2024-11-10 09:55:27 +0100

    Refactoring and commenting of code

--- main.py
+++ main.py
@@ -1,57 +1,103 @@
+import pygame_widgets as pg_widgets
 import pygame as pg
-import sys
 
-from algorithms import *
-from maze import *
-
-SIDEBAR_WIDTH = 400
-MAX_WINDOW_WIDTH = 1240 
-MAX_WINDOW_HEIGHT = 630
-MAX_MAZE_GRAPHICS_WIDTH = MAX_WINDOW_WIDTH - SIDEBAR_WIDTH
-
-FPS = 10
+from maze import * 
+from constants import *
+from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
 
 
-def get_user_input():
-    cell_width = int(input('Enter cell width: '))
-    max_maze_height = int(((MAX_WINDOW_HEIGHT / cell_width) - 3)/2)
-    max_maze_width = int(((MAX_MAZE_GRAPHICS_WIDTH / cell_width) - 3)/2)
-    maze_width = 2*int(input(f'Enter maze_width between 0 and {max_maze_width}: ')) + 3
-    maze_height = 2*int(input(f'Enter maze height between 0 and {max_maze_height}: ')) + 3
-    max_edge_weight = int(input('Enter max edge weight:'))
-    window_height = cell_width * maze_height
-    window_width = cell_width * maze_width + SIDEBAR_WIDTH
-    assert window_width <= MAX_WINDOW_WIDTH, 'WindowWidthError: Window to wide, chose smaller maze width.'
-    assert window_height <= MAX_WINDOW_HEIGHT, 'WindowHeightError: Window to tall, chose smaller maze height.'
-    assert window_width > 0, 'WindowWidthError: Window to small, chose larger maze width.'
-    assert window_height > 0, 'WindowHeightError: Window to small, chose larger maze height.'
-    return cell_width, maze_height, maze_width, window_height, window_width, max_edge_weight
 
+def main():
+    window = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pg.display.set_caption("Press S to start algorithm, and R to reset")
 
-def update_screen(maze, window):
-    maze.draw(window)
-    pg.display.flip()
+    # Create two sliders for adjusting the maze's width and height, each with a corresponding text box
+    height_slider = Slider(window, SLIDER_X_CORD, WINDOW_HEIGHT//4, SLIDER_LENGTH, 
+                           SLIDER_THICKNESS, min=MIN_MAZE_HEIGHT, max=MAX_MAZE_HEIGHT, step=2, initial=MIN_MAZE_HEIGHT)
+    width_slider = Slider(window, SLIDER_X_CORD, WINDOW_HEIGHT//2, SLIDER_LENGTH, 
+                          SLIDER_THICKNESS, min=MIN_MAZE_WIDTH, max=MAX_MAZE_WIDTH, step=2, initial=MIN_MAZE_WIDTH)
+    height_box = TextBox(window, BOX_X_CORD , WINDOW_HEIGHT//4 - 1.2*SLIDER_BOX_WIDTH, 
+                        SLIDER_BOX_WIDTH, SLIDER_BOX_WIDTH, fontSize=FONT_SIZE)
+    width_box = TextBox(window, BOX_X_CORD , WINDOW_HEIGHT//2 - 1.2*SLIDER_BOX_WIDTH, 
+                        SLIDER_BOX_WIDTH, SLIDER_BOX_WIDTH, fontSize=FONT_SIZE)
+    height_box.disable() # Makes boxes act only as labels, and not as input fields
+    width_box.disable()
 
-def check_user_quit(running):
-    for event in pg.event.get():
-        if event.type == pg.QUIT :
-            running = False
-
-    return running
-
-def main(fps):
-    pg.init()
-    CELL_WIDTH, MAZE_HEIGHT, MAZE_WIDTH, WINDOW_HEIGHT, WINDOW_WIDTH, MAX_EDGE_WEIGHT = get_user_input()
-    WINDOW = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    maze = Maze(MAZE_HEIGHT, MAZE_WIDTH, CELL_WIDTH, CELL_WIDTH, MAX_EDGE_WEIGHT, WINDOW, FPS)
-    kruskals(maze, WINDOW, fps)
     running = True
-    clock = pg.time.Clock()
+    maze_reseted = True
+    maze_width = MIN_MAZE_WIDTH
+    maze_height = MAX_MAZE_HEIGHT
     while running:
-        running = check_user_quit(running)
-        update_screen(maze, WINDOW)
-        clock.tick(fps)
-    sys.exit()
+        events = pg.event.get()
+        for event in events:
+
+            if event.type == pg.QUIT: 
+                running = False
+
+            if event.type == pg.KEYDOWN:  
+                if event.key == pg.K_s and maze_reseted: # Starts algorithm
+                    corner_x = MAX_GRAPHICAL_MAZE_WIDTH // 2 - (maze_width * CELL_WIDTH) // 2
+                    corner_y = WINDOW_HEIGHT // 2 - (maze_height * CELL_WIDTH) // 2
+                    maze = Maze(maze_height, maze_width, CELL_WIDTH, 30, corner_x, corner_y)
+                    maze.kruskals(window)
+                    maze_reseted = False
+                
+                if event.key == pg.K_r: # Resets the maze
+                    maze_reseted = True
+
+        
+        if maze_reseted: # Draws the initial maze layout without any algorithm applied
+            height_slider.value = adjust_slider(height_slider.value)
+            width_slider.value = adjust_slider(width_slider.value)
+            maze_width = width_slider.getValue()
+            maze_height = height_slider.getValue()
+            height_box.setText(height_slider.getValue())
+            width_box.setText(width_slider.getValue())
+                    
+    
+            window.fill((255, 255, 255))
+            draw_maze(window, maze_width, maze_height)
+            pg.draw.polygon(window, SIDE_BAR_COLOR,  # Draws sidebar
+                ((MAX_GRAPHICAL_MAZE_WIDTH, 0), (MAX_GRAPHICAL_MAZE_WIDTH + SIDEBAR_WIDTH, 0),
+                (MAX_GRAPHICAL_MAZE_WIDTH + SIDEBAR_WIDTH, WINDOW_HEIGHT), (MAX_GRAPHICAL_MAZE_WIDTH, WINDOW_HEIGHT)))
+
+            pg_widgets.update(events)
+            pg.display.update()
+
+
+#Draws maze in a checkered pattern
+def draw_maze(window, maze_width, maze_height):
+    graphical_corner_x = MAX_GRAPHICAL_MAZE_WIDTH // 2 - (maze_width * CELL_WIDTH) // 2
+    graphical_corner_y = WINDOW_HEIGHT // 2 - (maze_height * CELL_WIDTH) // 2
+
+    for y in range(maze_height):
+        for x in range(maze_width):
+            x_cord = graphical_corner_x + x * CELL_WIDTH
+            y_cord = graphical_corner_y + y * CELL_WIDTH
+            color = (-1,-1,-1)
+
+            if y % 2 == 0:  # If y is even, then alternate between path and undecided cells
+                if x % 2 == 1:
+                    color = MAZE_COLORS[UNDECIDED]
+                else:
+                    color = MAZE_COLORS[PATH]
+            else: # If y is odd, then alternate between undecided and wall cells
+                if x % 2 == 0:
+                    color = MAZE_COLORS[UNDECIDED]
+                else:
+                    color = MAZE_COLORS[WALL]
+        
+            pg.draw.polygon(window, color, 
+                            ((x_cord, y_cord),(x_cord + CELL_WIDTH, y_cord),
+                             (x_cord + CELL_WIDTH, y_cord + CELL_WIDTH), (x_cord, y_cord + CELL_WIDTH)))
+
+
+def adjust_slider(value): # Adjust sliders to display values following the formula 2k + 3
+    return value if (value - 3) % 2 == 0 else value - 1
+
 
 if __name__ == '__main__':
-    main(FPS)
+    pg.init()
+    pg.font.init()
+    main()
--- maze.py
+++ maze.py
@@ -1,87 +1,140 @@
 import pygame as pg
 import random
 
-class Cell:
-    colors = {'wall':(0,0,0), 'undecided':(0,150,150), 'path':(255,255,255), 
-              'edge_being_considered': (150,0,0),'connection_being_considered': (0,150,0)}
+from constants import * 
 
+
+class Cell:
     def __init__(self, object_type, x_cord, y_cord):
         self.object_type = object_type
-        self.x_cord = x_cord
-        self.y_cord = y_cord
+        self.x_cord = x_cord # Not in pixels
+        self.y_cord = y_cord 
     
+
     def convert_to_wall(self):
-        self.object_type = 'wall'
+        self.object_type = WALL
 
     def convert_to_path(self):
-        self.object_type = 'path'
+        self.object_type = PATH
 
 
+#Main datastructure
 class Maze:
-    def __init__(self, maze_height, maze_width, cell_width, cell_height, max_edge_weight, window, fps):
+    def __init__(self, maze_height, maze_width, cell_width, max_edge_weight, graphical_corner_x, graphical_corner_y):
         self.max_edge_weight = max_edge_weight
         self.width = maze_width
         self.height = maze_height
         self.cell_width = cell_width
-        self.cell_height = cell_height
-        self.grid = [[Cell('undecided', x, y) for x in range(self.width)] for y in range(self.height)]
-        self.place_out_walls()
-        self.place_out_paths()
+        self.graphical_corner_x = graphical_corner_x
+        self.graphical_corner_y = graphical_corner_y
+        self.grid = self.create_grid()
         self.edges = self.create_edges()
-        self.disjointed_set = self.create_disjointed_set()
-        
+        self.disjointed_set = Disjointed_set(self)
 
+        
+    #Draws maze from its top left corner position in the window
     def draw(self, window):
         for y in range(self.height):
             for x in range(self.width):
-                start_x, start_y = x * self.cell_width, y * self.cell_height
-                end_x, end_y = start_x + self.cell_width, start_y + self.cell_height
-                pg.draw.polygon(window, Cell.colors[self.grid[y][x].object_type],
+                start_x = x * self.cell_width + self.graphical_corner_x
+                start_y = y * self.cell_width + self.graphical_corner_y
+                end_x = start_x + self.cell_width 
+                end_y = start_y + self.cell_width
+
+                pg.draw.polygon(window, MAZE_COLORS[self.grid[y][x].object_type],
                 ((start_x, start_y), (end_x, start_y), (end_x, end_y), (start_x, end_y)))
 
-    def place_out_walls(self):
-        for y in range(1, self.height-1, 2):
-            for x in range(1, self.width-1, 2):
-                self.grid[y][x].convert_to_wall()
-    
-    def place_out_paths(self):
-        for y in range(0, self.height, 2):
-            for x in range(0, self.width, 2):
-                self.grid[y][x].convert_to_path()
-    
-    def create_edges(self):
-        edges = [] #[[node1, edge_cell, node2]]
-        for node1_ypos in range(0, self.height, 2):
-            for node1_xpos in range(0, self.width, 2):
-                if node1_xpos < self.width-1:
-                    horisontal_edge = [random.randint(1, self.max_edge_weight),self.grid[node1_ypos][node1_xpos],
-                                        self.grid[node1_ypos][node1_xpos+1], self.grid[node1_ypos][node1_xpos+2]]
-                    edges.append(horisontal_edge)
-                if node1_ypos < self.height-1:
-                    vertical_edge = [random.randint(1, self.max_edge_weight),self.grid[node1_ypos][node1_xpos],
-                                        self.grid[node1_ypos+1][node1_xpos], self.grid[node1_ypos+2][node1_xpos]]
-                    edges.append(vertical_edge)
-        edges.sort(key=lambda edge:edge[0])
-        return edges
 
-    def create_disjointed_set(self):
-        disjointed_set = Disjointed_set()
-        for row in self.grid:
-            for cell in row:
-                disjointed_set.parent[cell] = cell
-                disjointed_set.rank[cell] = 0
-        return disjointed_set
+    #Creates grid in a checkered pattern
+    def create_grid(self):
+        grid = [[] for _ in range(self.height)]
+        for y in range(self.height):
+            for x in range(self.width):
+               
+                if y % 2 == 0: #If y is even, then alternate between path and undecided cells
+                    if x % 2 == 1:
+                        grid[y].append(Cell(UNDECIDED, x, y))
+                    else:
+                        grid[y].append(Cell(PATH, x, y))
+                else:  #If y is odd, then alternate between undecided and wall cells
+                    if x % 2 == 0:
+                        grid[y].append(Cell(UNDECIDED, x, y))
+                    else:
+                        grid[y].append(Cell(WALL, x, y))
+        return grid
+
+
+    #Returns edges in the following format: [(weight, node1, edge_cell, node2)]
+    def create_edges(self):
+        edges = [] 
+        for node_y_cord in range(0, self.height, 2):
+            for node_x_cord in range(0, self.width, 2):
+
+                if node_x_cord < self.width-1: #Adds the path 2 cells to the right
+                    horisontal_edge = (random.randint(1, self.max_edge_weight),self.grid[node_y_cord][node_x_cord],
+                                        self.grid[node_y_cord][node_x_cord+1], self.grid[node_y_cord][node_x_cord+2])
+                    edges.append(horisontal_edge)
+
+                if node_y_cord < self.height-1: #Adds the path 2 cells down
+                    vertical_edge = (random.randint(1, self.max_edge_weight),self.grid[node_y_cord][node_x_cord],
+                                        self.grid[node_y_cord+1][node_x_cord], self.grid[node_y_cord+2][node_x_cord])
+                    edges.append(vertical_edge)
+
+        edges.sort(key=lambda edge:edge[0]) #Sorts edges based on the weight
+        return edges
+    
+
+    #Event handling and drawing of maze during kruskals algorithm 
+    def animate_kruskals(self, clock, window):
+        self.draw(window)
+        pg.display.update()
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT: 
+                pg.quit() 
+    
+
+    #Kruskals algorithm, changes the colors of the cells being processed
+    def kruskals(self, window):
+        clock = pg.time.Clock()
+        for _ , cell1, edge_cell, cell2 in self.edges:
+
+            edge_cell.object_type = EDGE_BEING_PROCESSED
+            cell1.object_type = CONNECTION_BEING_PROCESSED
+            cell2.object_type = CONNECTION_BEING_PROCESSED
+            self.animate_kruskals(clock, window)
+
+            if self.disjointed_set.union(cell1, cell2):
+                edge_cell.convert_to_path()
+            else:
+                edge_cell.convert_to_wall()
+            
+            cell1.object_type = PATH
+            cell2.object_type = PATH
+            
+            reset = self.animate_kruskals(clock, window)
+
 
 class Disjointed_set:
-    def __init__(self):
+    #Creates disjointed set and fill it with the cells of the maze
+    def __init__(self, maze):
         self.parent = {}
         self.rank = {}
 
+        for row in maze.grid:
+            for cell in row:
+                self.parent[cell] = cell
+                self.rank[cell] = 0
+
+
+    #Connects two cells if they are unconnected in the maze
     def union(self, cell1, cell2):
         parent1 = self.find(cell1)
         parent2 = self.find(cell2)
+
         if parent1 == parent2:
             return False
+        
         if self.rank[parent1] == self.rank[parent2]:
             self.rank[parent1] += 1
             self.parent[parent2] = parent1
@@ -89,8 +142,11 @@ class Disjointed_set:
             self.parent[parent2] = parent1
         else:
             self.parent[parent1] = parent2
+
         return True
 
+
+    #Finds root of set with path compression.
     def find(self, cell):
         if self.parent[cell] == cell:
             return cell
